0454747a0ab0f71c61d783d47142437ac15e013c
TH-Projects <mail@timoheckmann.de>
2024-09-25 17:54:19 +0200

    Introduced python logging module for logging process of compiling

--- Compiler/forgeapi_compiler.py
+++ Compiler/forgeapi_compiler.py
@@ -1,6 +1,7 @@
 import copy
 import sys
 import os
+import logging
 from CompilerFrontend.Lexer.lexer import Lexer
 from CompilerFrontend.Parser.parser import Parser
 from CompilerFrontend.Parser.print_tree import PrintTree
@@ -9,16 +10,21 @@ from CompilerBackend.nodejs_endpoint_generator import NodeJSCodeGenerator
 from CompilerBackend.appjs_route_generator import AppJSRouteGenerator
 from CompilerBackend.env_generator import EnvGenerator
 
+# Logging-Konfiguration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def check_arguments():
     """
     Check command line arguments for the source file
     :return: The path to the source file
     """
     if len(sys.argv) != 2:
-        sys.exit("Error: Compiler needs source file as argument.")
+        logging.error("Compiler needs source file as argument.")
+        sys.exit(1)
     file_path = sys.argv[1]
     if not file_path.endswith('.forgeapi'):
-        sys.exit("Error: The source file must have a .forgeapi extension.")
+        logging.error("The source file must have a .forgeapi extension.")
+        sys.exit(1)
     return file_path
 
 def read_source_file(file_path):
@@ -30,7 +36,8 @@ def read_source_file(file_path):
         with open(file_path, 'r') as inputFile:
             return inputFile.read()
     except FileNotFoundError:
-        sys.exit(f"Error: File {file_path} not found.")
+        logging.error(f"File {file_path} not found.")
+        sys.exit(1)
 
 def initialize_lexer(source):
     """
@@ -51,7 +58,8 @@ def parse_tokens(tokens):
     try:
         return parser.parse()  # Parse the tokens to create the parse tree
     except SyntaxError as e:
-        sys.exit(f"Syntax error during parsing: {e}")
+        logging.error(f"Syntax error during parsing: {e}")
+        sys.exit(1)
 
 def extract_endpoint_data(parse_tree):
     """
@@ -72,15 +80,6 @@ def process_database_schema(parse_tree):
         del database_schema['rest_block']
     return database_schema
 
-def generate_sql_code(database_schema):
-    """
-    Generate SQL code from the database schema
-    :param database_schema: The database schema 
-    :return: The generated SQL code
-    """
-    sql_generator = SQLCodeGenerator(database_schema)
-    return sql_generator.generate()
-
 def write_sql_to_file(sql_code, output_file_path):
     """
     Write the generated SQL code to a file, overwriting it if it exists
@@ -88,13 +87,13 @@ def write_sql_to_file(sql_code, output_file_path):
     :param output_file_path: The path to the output file
     """
     try:
-        # Write the SQL code to the file
-        print(f"Writing SQL code to {output_file_path}")
+        logging.info(f"Writing SQL code to {output_file_path}")
         with open(output_file_path, 'w') as outputFile:
             outputFile.write(sql_code)
-        print(f"SQL code successfully written to {output_file_path}")
+        logging.info(f"SQL code successfully written to {output_file_path}")
     except IOError as e:
-        sys.exit(f"Error writing to file: {e}")
+        logging.error(f"Error writing to file: {e}")
+        sys.exit(1)
 
 def print_endpoint_data(endpoint_data):
     """
@@ -144,10 +143,10 @@ def write_endpoints_to_files(endpoint_data, endpoint_output_dir):
             try:
                 with open(file_path, 'w') as file:
                     file.write(endpoint['generated_code'])
-                print(f"File '{file_name}' was created in '{table_name}' folder")
+                logging.info(f"File '{file_name}' was created in '{table_name}' folder")
             except IOError as e:
-                sys.exit(f"Error writing to file: {e}")
-            
+                logging.error(f"Error writing to file: {e}")
+                sys.exit(1)           
 
 def write_env_to_file(env_content, env_file_path):
     """
@@ -156,15 +155,15 @@ def write_env_to_file(env_content, env_file_path):
     :param env_file_path: The path to the output file
     """
     try:
-        # Write the environment variables to the file
         with open(env_file_path, 'w') as outputFile:
             outputFile.write(env_content)
-        print(f"Environment variables successfully written to {env_file_path}")
+        logging.info(f"Environment variables successfully written to {env_file_path}")
     except IOError as e:
-        sys.exit(f"Error writing to file: {e}")
+        logging.error(f"Error writing to file: {e}")
+        sys.exit(1)
 
 def main():
-    print("ForgeAPI Compiler")
+    logging.info("ForgeAPI Compiler started")
     
     file_path = check_arguments()
     source = read_source_file(file_path)
@@ -178,7 +177,8 @@ def main():
     database_schema = process_database_schema(parse_tree)
 
     # Generate SQL code
-    sql_code = generate_sql_code(database_schema)
+    sql_generator = SQLCodeGenerator(database_schema)
+    sql_code = sql_generator.generate()
     output_file_path = "./DB/schema.sql"
     write_sql_to_file(sql_code, output_file_path)
 
@@ -200,13 +200,14 @@ def main():
     app_js_generator = AppJSRouteGenerator(app_js_path)
     app_js_generator.register_routes(endpoint_data)
 
-    
     # Print the formatted parse tree for debugging
     #printed_tree = PrintTree(parse_tree)
     #print(printed_tree)
 
     # Print endpoint data and generated code for debugging
     #print_endpoint_data(nodejs_code)
+
+    logging.info("ForgeAPI Compiler finished successfully")
 
 if __name__ == "__main__":
     main()
