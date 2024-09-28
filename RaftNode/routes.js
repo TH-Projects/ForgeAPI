3970b25bdb7fa41b583f0b98e126af29c87b35ab
Timo Heckmann <mail@timoheckmann.de>
2024-09-28 19:58:22 +0200

    Removed generated code example from repo

--- RaftNode/routes.js
+++ RaftNode/routes.js
@@ -1,10 +1,1 @@
-module.exports = async function (fastify) {
-    fastify.register(require('./REST/employees/getAllEmployees'));
-    fastify.register(require('./REST/employees/getEmployeesByName'));
-    fastify.register(require('./REST/employees/postEmployees'));
-    fastify.register(require('./REST/employees/putEmployees'));
-    fastify.register(require('./REST/employees/deleteEmployee'));
-    fastify.register(require('./REST/departments/getDepartments'));
-    fastify.register(require('./REST/departments/postDepartments'));
-    fastify.register(require('./REST/departments/deleteDepartments'));
-};
+// Generated routes will be placed here
